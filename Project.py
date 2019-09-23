c47e4619320c3a331f21d90884e7b9e4f585c01a
Adriana Mundo <adrianamundog@hotmail.com>
2019-09-22 23:57:22 -0600

    Parte 2 4 incisos hechos

--- Project.py
+++ Project.py
@@ -4,7 +4,7 @@ import requests,sys,csv,json
 
 #lista de url's a usar
 url1="http://ufm.edu/Portal"
-#url2
+url2 = "http://ufm.edu/Estudios"
 #url3
 #url4
 
@@ -102,6 +102,48 @@ print("-------------------------------------------------------------------------
 print("=======================================================================================================")
 print("2.Estudios")
 
+#now navigate to  /Estudios (better if you obtain href from the DOM)
+try:
+    html_content = requests.get(url2).text
+except:
+    print(f"unable to get {url2}")
+    sys.exit(1)
+
+soup = BeautifulSoup(html_content, "html.parser")
+
+#display all items from "topmenu" (8 in total)
+print("Display all items from topmenu:")
+b = 0
+tabla = soup.find("div", { "id" : "topmenu" })
+for datos in tabla.findAll("li"):
+# for datos in tabla.findAll("a",{"class":"external text"}):
+      celda = datos.text
+      b += 1
+      print(b,"<",celda,">")
+print("-------------------------------------------------------------------------------------------------------")
+
+#display ALL "Estudios" (Doctorados/Maestrias/Posgrados/Licenciaturas/Baccalaureus)
+print("Display all Estudios:")
+tablas1 = soup.find("div",{"id":"mw-content-text"})
+for datos in tablas1.findAll("div",{"class":"estudios"}):
+        celdas = datos.text
+        print("-",celdas)
+print("-------------------------------------------------------------------------------------------------------")
+
+#display from "leftbar" all &lt;li> items (4 in total)
+print("Display from leftbar all &lt;li> items:")
+c=0
+tablas2 = soup.find("div",{"class":"leftbar"})
+for datos in tablas2.findAll("li"):
+#for datos in tablas2.findAll("a",{"class":"external text"}):
+        celdas2 = datos.text
+        c += 1
+        #print(celdas2)   
+        print(c,"<",celdas2,">")
+print("-------------------------------------------------------------------------------------------------------")
+
+#get and display all available social media with its links (href) "class=social pull-right"
+print("Get and display all available social media with its links (href) class =social pull -right:")
 
 
 #for div in soup.find_all("div"):
